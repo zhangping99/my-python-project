b44cdbb05f14426eb6d443a21e87a566c20ca013
zhangping99 <1306179942@qq.com>
2025-09-16 22:31:51 +0800

    first cot

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 import pytest
 from calculator import add, multiply
 def test_add():
-    assert add(2, 3) == 6
+    assert add(2, 3) == 5
     assert add(-1, 1) == 0
 def test_multiply():
     assert multiply(3, 4) == 12
