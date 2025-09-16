b2133690fb143fdd214ce70413b9aea0369e5d56
zhangping99 <1306179942@qq.com>
2025-09-16 22:29:54 +0800

    t

--- test_calculator.py
+++ test_calculator.py
@@ -4,5 +4,5 @@ def test_add():
     assert add(2, 3) == 6
     assert add(-1, 1) == 0
 def test_multiply():
-    assert multiply(3, 4) == 15
+    assert multiply(3, 4) == 12
     assert multiply(2, 0) == 0
